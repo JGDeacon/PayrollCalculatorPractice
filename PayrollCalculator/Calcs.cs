e00ab7c868db23fb46d47b738b8260938feae5ad
JGDeacon <deacon6878@gmail.com>
2021-03-10 22:48:52 -0500

    Add project files.

---
+++ PayrollCalculator/Calcs.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollCalculator
+{
+    class Calcs
+    {
+        
+
+        public decimal GetGross(string userName)
+        {
+            decimal grossPay = 0.00m; // variable scoped to method
+            Console.WriteLine($"How much would you like to gross per week {userName}?");
+            // By typing "Try" then hitting Tab Tab you get the try block below. This will allow code to run, but if it crashes the program can recover.
+            // We are doing this for the gross income question to prevent a crash if the user put in non numeric charecters. Try/Catches can do a number of things.
+            try
+            {
+
+                //We are trying to parse (convert) a string to numbers
+                grossPay = Decimal.Parse(Console.ReadLine());
+                return grossPay; 
+            }
+            catch (Exception)
+            {
+                //this is the code that runs if the user enters something that can't be parse (converted) into a number
+                Console.WriteLine("I'm sorry we couldn't accept that. Please only user numbers and a decimal. For example 2564.25.");
+
+                return grossPay;
+            }
+        }
+
+        public decimal getWage(string userName)
+        {
+            decimal wage = 0.00m; // variable scoped to method
+            Console.WriteLine($"{userName} how much do you make an hour?");
+            try
+            {
+                wage = Decimal.Parse(Console.ReadLine());
+                return wage;
+            }
+            catch (Exception)
+            {
+                //this is the code that runs if the user enters something that can't be parse (converted) into a number
+                Console.WriteLine("I'm sorry we couldn't accept that. Please only user numbers and a decimal. For example 15.75");
+                return wage;
+            }
+        }
+
+        public bool isOverTime()
+        {
+            Console.WriteLine("Do you get time and a half for overtime hours? (y/n)");
+            //Doing a few things here. Setting the value of overTime 
+            string overTime = Console.ReadLine();
+
+            //Because Ternaries are fun... lets use one and setting the value of overTime to lowercase
+            bool answer = (overTime.ToLower() == "y") ? true : false;
+            return answer;
+
+            //notice their isn't a try block around this code. You can input anything into a string so there isn't going to be a parse error.
+            //to idiot proof this you would want to validate the value of overTime to make sure the user entered a y or n
+            //Users are very creative when it comes to finding the "non-happy path" of an application.
+        }
+
+        public decimal HowManyHours(decimal wage, decimal goal, bool overTime)
+        {
+            //Declare and initialize the hours total remember to add the m to decimals when YOU set the value directly
+            decimal totalHours = 0.00m;
+            //math time!
+            if ((goal/wage) < 40) //Do we need OT to meet our goal?
+            {
+                totalHours = goal / wage;
+                return totalHours;
+            }
+            else
+            {
+                if (overTime)
+                {
+                    totalHours = (goal - (wage * 40)) / (wage * 1.5m)+40;
+                    //totalHours can be messy so lets round at the 2nd decimal place, but we are going to do that while presenting back to the user
+                    return totalHours;
+                }
+                else
+                {
+                    totalHours = goal / wage;
+                    return totalHours;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ PayrollCalculator/PayrollCalculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollCalculator
+{
+    class PayrollCalculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This application will figure out how many hours you need to work to meet your gross income goal for the week");
+            Console.WriteLine("May I have you name?");
+            //We are going to declare a string variable and initialize it with a Console.ReadLine()
+            string userName = Console.ReadLine();
+            //We are going to declare a few more variables now and we will initialize them later
+
+            Calcs calcs = new Calcs();
+            // Get Gross Pay
+            decimal grossPay = calcs.GetGross(userName);
+            //mitigation... this is a bit heavy handed in its application, but they are stuck in the loop until they provide a usable value
+            while (grossPay == 0.00m)
+            {
+                grossPay = calcs.GetGross(userName);
+            }
+
+            // Get Hourly Wage
+            decimal hourlyWage = calcs.getWage(userName);
+            //we will be nicer on this one and only let them make 10 mistakes
+            int counter = 1;
+            while (hourlyWage == 0.00m && counter < 10)
+            {
+                hourlyWage = calcs.getWage(userName);
+               counter++;
+            }
+            //However we need an hourly wage. Without it we divide by 0 and that doesn't work. So lets...
+            if (counter == 10)
+            {
+                Console.WriteLine("We set your hourly wage to $10.00");
+                hourlyWage = 10.00m;
+            }
+            //Do you get time an a half?
+            bool isOverTime = calcs.isOverTime();
+
+            //This one does the magic. We take the other values collected and pass them into this method to get the total hours needed to meet the Gross pay goal.
+            decimal hoursNeeded = calcs.HowManyHours(hourlyWage, grossPay, isOverTime);
+            //Using .ToString with a mask to round the decimal to 2 places
+
+            Console.WriteLine(); 
+            Console.WriteLine($"I don't know if its good or bad {userName} but it looks like you will need to work {hoursNeeded.ToString("0.##")} hours a week to meet your goal");
+            //There has got to be a better way to space the outputs
+            Console.WriteLine();
+            Console.WriteLine();
+            //C2 in a ToString will format the decimal value as currency like $1,234.56
+            Console.WriteLine($"You entered {hourlyWage.ToString("C2")} for your hourly wage, and you answered {isOverTime.ToString()} to the over time question");
+            Console.WriteLine(userName + " is those entries are incorrect please run the program again with the correct entries");
+            Console.WriteLine("Would you like to run the application again? y/n");
+            
+            //Just to show it this time we are using to upper and running the whole thing in an if condition. True we restart the program by rerunning the Main method
+            if (Console.ReadLine().ToString().ToUpper() == "Y")
+            {
+                //Clear out the console window
+                Console.Clear();
+                Main(args); //No idea what args are.... YET!
+            }
+
+            //They said no and the program closes.
+        
+        }
+    }
+}
